8b4795032fcb0730c3faf15d02820393ab207d39
Aryan Sopan Surve <aryansurve1210@gmail.com>
2026-09-14 10:04:31 +0530

    Change Python version and add migration commands
    Updated Python version and added migration and static collection commands.

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-FROM python:3.12-slim
+FROM python:3.11-slim
 
 WORKDIR /app
 
@@ -10,6 +10,9 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+
+RUN python manage.py migrate --noinput
+RUN python manage.py collectstatic --noinput
 
 EXPOSE 8000
 
