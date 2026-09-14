fcb5fbcf3ab5c07f47e39506642ee46e5f465cdb
Aryan Sopan Surve <aryansurve1210@gmail.com>
2026-09-14 09:42:59 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Running database migrations..."
+
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+
+exec gunicorn employee_pro.wsgi:application \
+    --bind 0.0.0.0:8000
